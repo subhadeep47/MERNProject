5da0f72a4127696193ab3e707a04b24aea0b5f52
subhadeep <subhadeepsaha47@gmail.com>
2024-12-15 13:49:55 +0530

    cors issue fixed

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
 
 //for securing the important file in config.env
@@ -6,6 +7,7 @@ const dotenv = require('dotenv');
 dotenv.config({path:'./config.env'});
 
 //using middlewires for all routing paths using express router
+app.use(cors({ origin: ['http://localhost:3000', 'https://social-app-backend-vcwx.onrender.com', 'http://social-app-backend-vcwx.onrender.com'], credentials: true }));
 app.use(require('./router/route'));
 
 PORT = process.env.PORT
