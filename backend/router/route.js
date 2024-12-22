12532de987e459607bb3aa236200f273c043dff7
subhadeep <subhadeepsaha47@gmail.com>
2024-12-22 13:44:32 +0530

    added route

--- backend/router/route.js
+++ backend/router/route.js
@@ -11,6 +11,9 @@ const UserAuth = require('../middleware/UserAuthenticate');
 route.use(cookieParser());
 route.use(express.json());
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 route.get('/',Check, (req,res)=>{
     console.log('from path');
